b132d1c5d7bf11485bc02ececd44e88d9aaf76ae
jwhector <jared.hector@gmail.com>
2021-11-30 01:51:37 -0800

    Updates

---
+++ precache-manifest.729cdc183507f239d673be6ff7398c95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb7c70ed2ff59f428749dc9021ce365",
+    "url": "/book-search-engine/index.html"
+  },
+  {
+    "revision": "95c11fa89618f795558e",
+    "url": "/book-search-engine/static/css/2.429e79c4.chunk.css"
+  },
+  {
+    "revision": "ea0d1145d7b736b09cf9",
+    "url": "/book-search-engine/static/css/main.10507661.chunk.css"
+  },
+  {
+    "revision": "95c11fa89618f795558e",
+    "url": "/book-search-engine/static/js/2.c51c8287.chunk.js"
+  },
+  {
+    "revision": "4ebd2e1c8aec2794a625d08d8b03eea9",
+    "url": "/book-search-engine/static/js/2.c51c8287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea0d1145d7b736b09cf9",
+    "url": "/book-search-engine/static/js/main.98f10ffc.chunk.js"
+  },
+  {
+    "revision": "949165f3c8b71fcd8709",
+    "url": "/book-search-engine/static/js/runtime-main.e855f5ac.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.71320915d3319e6c596b819d291acad8.js"
+  "/book-search-engine/precache-manifest.729cdc183507f239d673be6ff7398c95.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/book-search-engine/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
