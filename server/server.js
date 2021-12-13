26c66dc0fa12cc3860632a9db0613590e8ef2ab6
jwhector <jared.hector@gmail.com>
2021-12-13 12:38:32 -0800

    Add serving of front end

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-// const routes = require('./routes');
+const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const cors = require('cors');
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3001;
 
 const startServer = async () => {
   var corsOptions = {
-    origin: 'http://localhost:3000',
+    origin: '*',
     credentials: true
   };
   const server = new ApolloServer({
@@ -31,17 +31,16 @@ const startServer = async () => {
   app.use(cors(corsOptions))
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-
+  
   // if we're in production, serve client/build as static assets
   if (process.env.NODE_ENV === 'production') {
+    console.log(path.join(__dirname, '../client/build'));
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
+    
+  app.use(routes);
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
 
-  // app.use(routes);
 
   db.once('open', () => {
     app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
